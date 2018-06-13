81ae16474e379613a57949a8e972927a364c1249
hColleen <hcolleen@gmail.com>
2018-06-13 03:03:41 -0700

    set up object prototypes

--- js/app.js
+++ js/app.js
@@ -1,35 +1,49 @@
+//tutorial here https://www.youtube.com/watch?v=7PHhRrjgTDA and previous game created here https://www.khanacademy.org/computer-programming/dragon-and-knight/6160202051
+//note: KhanAcademy game credits also apply
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function(x, y, speed) {
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+    this.sprite = 'images/char-boy.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
 };
 
-// Draw the enemy on the screen, required method for game
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(x, y){
+	this.x = x;
+	this.y = y;
+	this.sprite = "images/char-cat-girl.png";
+};
+
+Player.prototype.update = function(dt){
+	
+};
+
+Player.prototype.render = function(){
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress){
+	
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];
 
+
+
+
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,7 @@ var Engine = (function(global) {
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
-        'images/enemy-bug.png',
+        'images/char-cat-girl.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
